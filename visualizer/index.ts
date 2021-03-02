d821051825ae6f63de2f2485f5606d45650da3be
Jeremy Mechen <jeremymechen1997@gmail.com>
2021-03-02 17:19:27 +1300

    Add ability to add nodes

--- visualizer/index.ts
+++ visualizer/index.ts
@@ -15,10 +15,14 @@ interface Grid {
 }
 
 const input: Factorio_Node[] = [
-    { id: 0, type: 'input', connection: [ 1 ] },
-    { id: 1, type: 'connector', connection: [ 2, 3 ] },
-    { id: 2, type: 'output', connection: [] },
-    { id: 3, type: 'output', connection: [] }
+    { id: 0, type: 'input', connection: [ 2 ] },
+    { id: 1, type: 'input', connection: [ 3 ] },
+    { id: 2, type: 'connector', connection: [ 4, 5 ] },
+    { id: 3, type: 'connector', connection: [ 6, 7 ] },
+    { id: 4, type: 'output', connection: [] },
+    { id: 5, type: 'output', connection: [] },
+    { id: 6, type: 'output', connection: [] },
+    { id: 7, type: 'output', connection: [] }
   ]
 
 function initialize_empty_grid(size_x: number, size_y: number): Grid {
@@ -26,31 +30,83 @@ function initialize_empty_grid(size_x: number, size_y: number): Grid {
     for (let i = 0; i < size_x; i++){
         grid.squares[i] = []
         for (let j = 0; j < size_y; j++){
-            grid.squares[i][j] = {type: '  Empty '}
+            grid.squares[i][j] = {type: ' '}
         }
     }
     return grid
 }
 
-function fill_grid(input: Factorio_Node[], grid: Grid) {
-    var filled_grid: Grid = grid
+function grid_filled(input: Factorio_Node[], grid: Grid) {
+    var mutated_grid: Grid = grid
 
-    // Iterate over every input
-    input.forEach(node => {
-        // Create the transport belt for the input
-        if (node.type == 'input') {
-            grid.squares[grid.size_y - 1][0].type = '  Belt  '
-        }
+    const spacing_x = 3
+    const spacing_y = 3
 
-        var connection_node = input[node.connection[0]]
-        grid.squares[grid.size_y - 2][0].type = 'Splitter'
-        grid.squares[grid.size_y - 2][1].type = 'Splitter'
+    const input_nodes: Factorio_Node[] = input.filter((node) => {
+        return node.type == 'input'
+    })
+    const connector_nodes: Factorio_Node[] = input.filter((node) => {
+        return node.type == 'connector'
+    })
+    const output_nodes: Factorio_Node[] = input.filter((node) => {
+        return node.type == 'output'
+    })
 
-        grid.squares[grid.size_y - 3][0].type = ' Output '
-        grid.squares[grid.size_y - 3][1].type = ' Output '
+    input_nodes.forEach((node, index) => {
+        mutated_grid = grid_with_input_node(grid, spacing_x * index, grid.size_y - 1)
+    });
+
+    connector_nodes.forEach((node, index) => {
+        mutated_grid = grid_with_splitter_node(grid, spacing_x * index, grid.size_y - spacing_y - 1)
+    });
+
+    output_nodes.forEach((node, index) => {
+        mutated_grid = grid_with_output_node(grid, spacing_x * index, grid.size_y - 2 * spacing_y - 1)
     });
     
-    return filled_grid
+    return mutated_grid
+}
+
+function grid_with_input_node(grid: Grid, pos_x: number, pos_y: number): Grid {
+    var mutated_grid: Grid = grid
+
+    if (squares_are_empty([grid.squares[pos_y][pos_x]])) {
+        mutated_grid.squares[pos_y][pos_x].type = 'I'
+    }
+
+    return mutated_grid
+
+}
+
+function grid_with_splitter_node(grid: Grid, pos_x: number, pos_y: number): Grid {
+    var mutated_grid: Grid = grid
+
+    if (squares_are_empty([grid.squares[pos_y][pos_x], grid.squares[pos_y][pos_x + 1]])) {
+        mutated_grid.squares[pos_y][pos_x].type = 'S'
+        mutated_grid.squares[pos_y][pos_x + 1].type = 'S'
+    }
+
+    return mutated_grid
+}
+
+function grid_with_output_node(grid: Grid, pos_x: number, pos_y: number): Grid {
+    var mutated_grid: Grid = grid
+
+    if (squares_are_empty([grid.squares[pos_y][pos_x]])) {
+        mutated_grid.squares[pos_y][pos_x].type = 'O'
+    }
+
+    return mutated_grid
+
+}
+
+function squares_are_empty(squares: Square[]) {
+    squares.forEach(square => {
+        if (square.type != ' ') {
+            return false
+        }
+    });
+    return true
 }
 
 function display_grid(grid: Grid) {
@@ -58,12 +114,12 @@ function display_grid(grid: Grid) {
         var formatted_line: string = ""
         for (let j = 0; j < grid.squares[i].length; j++){
             const square = grid.squares[i][j]
-            formatted_line += "| " + square.type + " "
+            formatted_line += square.type + "|"
         }
-        console.log(formatted_line + "|")
+        console.log("|" + formatted_line)
     }
 }
 
-var grid = initialize_empty_grid(5, 5)
-var filled_grid = fill_grid(input, grid)
-display_grid(grid)
+const empty_grid = initialize_empty_grid(20, 20)
+const filled_grid = grid_filled(input, empty_grid)
+display_grid(filled_grid)
